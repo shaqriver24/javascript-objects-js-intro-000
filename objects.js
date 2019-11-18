0f54def3a84c124c5387dfcba5de2c13e7a5d2d9
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-18 00:00:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  Object.assign(playlist, {artistiname})
+  Object.assign(playlist, {artistName})
 
   delete playlist.artistName
 
